e0d18d8187451a42aa8c1bcd91ab911f338bc741
7amady100 <75930823+7amady100@users.noreply.github.com>
2021-11-28 22:19:31 +0200

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,141 @@
+import os
+import os
+try:
+    import uuid
+except:
+    os.system('pip install uuid')
+
+try:
+    from random import *
+except:
+    os.systeam('pip install random ')
+
+try:
+    import string
+except:
+    os.system('pip install string')
+
+try:
+    import requests
+except:
+    os.system('pip install requests ')
+
+try:
+    from user_agent import generate_user_agent
+except:
+    os.system('pip install user_agent ')
+
+try:
+    from datetime import datetime
+except:
+    os.system('pip install datetime ')
+
+try:
+    import time
+except:
+    os.system('pip install time')
+else:
+    os.system('clear')
+    try:
+        import pyfiglet
+    except:
+        os.system('pip install pyfiglet')
+    else:
+        os.system('clear')
+        import pyfiglet, os
+        from time import sleep
+        import webbrowser
+        Z = '\x1b[2;31m'
+        G = '\x1b[1;32m'
+        A = '\033[2;39m'
+        C = '\033[2;35m' 
+        B = '\033[2;36m'
+        C = '\033[2;35m' 
+        sleep(0.1)
+        
+        P55 = pyfiglet.figlet_format('TROX')
+        print( C + P55)
+        sleep(2)
+        
+        os.system('clear')
+        P55 = pyfiglet.figlet_format('========>')
+        print(  B + P55)
+        sleep(1)
+        
+        
+        P55 = pyfiglet.figlet_format('========>')
+        print(  C + P55)
+        sleep(2)
+        webbrowser.open('https://youtube.com/channel/UCv6zO8lxsGPWsthmIOJ_4_A')
+         
+        P55 = pyfiglet.figlet_format('========>')
+        print(  G + P55)
+        sleep(2)
+        
+        
+        P55 = pyfiglet.figlet_format('========>')
+        print(  B + P55)
+        sleep(5)
+        
+         
+        os.system('clear')
+        print(G+  """
+================================
+المطور TRX
+================================
+المكاتب انتضر يتم تثبيت ✈️⛏️
+================================
+        """)
+        webbrowser.open('https://t.me/FAYREWS')
+print(" تثبيت مكاتب تيجرام قناتي @FAYREWS")
+print("$"*60)
+os.system('pip install requests')
+os.system('pip install pyfiglet')
+os.system('pip install sys')
+os.system('pip install py_compile')
+os.system('pip install youtube_dl')
+os.system('pip install uuid')
+os.system('pip install time')
+os.system('pip install os')
+os.system('pip install random')
+os.system('pip install datetime')
+os.system('pip install string')
+os.system('pip install secrets')
+os.system('pip install webbrowser')
+os.system('pip install hashlib')
+os.system('pip install colorama')
+os.system('pip install n')
+os.system('pip install mm')
+os.system('pip install new')
+os.system('pip install sleep')
+os.system('pip install BeautifulSoup')
+os.system('pip install new')
+os.system('pip install pafy')
+os.system('pip install bs4')
+os.system('pip install sys')
+os.system('pip install json')
+os.system('pip install random')
+os.system('pip install uuid')
+os.system('pip install secrets')
+os.system('pip install datetime')
+os.system('pip install *')
+os.system('pip install argparse')
+os.system('pip install InstagramAPI')
+os.system('pip install sleep')
+os.system('pip install string')
+os.system('pip install uuid4')
+os.system('pip install generate_user_agent')
+os.system('pip install threading')
+os.system('pip install json')
+os.system('pip install datetime')
+os.system('pip install token_hex')
+os.system('pip install secrets')
+os.system('pip install token_hex')
+os.system('pip install Fore')
+os.system('pip install secrets')
+os.system('pip install uuid')
+os.system('pip install re')
+os.system('pip install b')
+os.system('cls' if os.name == 'nt' else 'clear') 
+print("≠"*60)
+print("تم اكتمال جميع المكاتب الان يمكن استخدام اي اداة بدون مشكله او طلب منك تثبيت مكتبه")
